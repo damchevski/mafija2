8cbe3bdf5126800d6f284f0e2cb462fae04a2ea2
Vasil Trendafilov <30906657+vasetrendafilov@users.noreply.github.com>
2018-06-23 13:23:25 +0200

    oop updejt

--- app/Models/Energy.php
+++ app/Models/Energy.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Energy Extends Model
 {
@@ -11,5 +12,17 @@ class Energy Extends Model
 		'energija',
     'status'
 	];
+	public function calculateEnergy(){
+		if($this->energija <=100){
+			 //razlika vo sekundi za vreminja
+			 $difference_in_seconds = strtotime(Carbon::now()) - strtotime($this->updated_at);
+			 $addEnergija = $difference_in_seconds / 10;//na kolku sekundi se polni eden poen
+			 if($this->energija + $addEnergija < 100){
+					$this->update(['energija' => $this->energija + $addEnergija ]);
+			 }else {
+					$this->update(['energija' => 100 ]);
+			 }
+		}
+	}
 
 }
